cbcd60f4eaa6fdf47a35d2e5503cab6cbfd506e8
Mladen Draganović <mladenoneacc@gmail.com>
2021-11-07 03:39:01 +0100

    🔧 Removed setup for GitHub Pages

--- docs/assets/icon-menu.57281e8d.js
+++
@@ -1,1 +0,0 @@
-var s="/assets/icon-menu.7b698e49.svg";export{s as _};
--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,6 @@ import eslintPlugin from 'vite-plugin-eslint'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), eslintPlugin()],
-  build: {
-    outDir: 'docs'
-  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, '/src'),
